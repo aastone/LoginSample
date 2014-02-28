b4fa403149095bc6900211fc44460738d2c9f0e7
stone <stoneiswang@gmail.com>
2014-02-28 13:18:05 +0800

    Initial Commit

---
+++ LoginSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LoginSample
+//
+//  Created by wangyipu on 14-2-28.
+//  Copyright (c) 2014年 stone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
